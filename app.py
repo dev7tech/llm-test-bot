ed9da26150bd6b864fb716af695f93cb331e726f
jacob647548 <jacob.devtech102@gmail.com>
2024-06-19 02:23:31 -0600

    [Upgrade] test short term memory

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ from datetime import datetime
 import pytz
 import os
 
-open_ai = AsyncOpenAI(api_key=openai_api_key)
-url: str = f"https://{supabase_url}.supabase.co"
-key: str = supabase_key
+open_ai = AsyncOpenAI(api_key="sk-proj-QAKYkuXwcgLdih99nJ9lT3BlbkFJmLMfrcezDHUvsV2TkK5d")
+url: str = "https://ghmpkmcrsedpizfwgcqr.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdobXBrbWNyc2VkcGl6ZndnY3FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NDYyMDcsImV4cCI6MjAyMzUyMjIwN30.xZLK0E6bjfy6UoKqwZkZCs38L6uXJd8-VkknTmX2ULg"
 supabase: Client = create_client(url, key)
 
-userid = "f018ee62-9fa5-5f34-9e62-724f1b40f75a"
-
+userid = "2a5d81a4-f09d-5852-9cb6-41281f9827ef"
 
 def get_anon_profile():
   response = supabase.table('anon_profiles').select('*').eq('id', userid).execute()
   return response.data[0]
 
 def get_ai_profiles():
-  response = supabase.table('ai_profiles').select('*').execute()
+  response = supabase.table('ai_profiles').select('*').order('created_at', desc=True).execute()
   return response.data
 
 def get_latest_message_time():
@@ -35,6 +34,16 @@ def check_sleep_order():
     return True
   else:
     return False   
+
+def generate_scenario():
+  scenario = f"""
+  Your name is {username}, a 32-year-old man who enjoys reminiscing about past experiences.
+  You've just encountered a {ai_profile['age']} year old {ai_profile['gender']} named {ai_profile['name']} on a Tinder-like app.
+  You want to great warmly and then share a memorable experience that reveals something personal about you, creating a connection.
+  After sharing the memory, you want to ask a few questions to see if {ai_profile['name']} remembers what you said.
+  Make sure the conversation flows naturally and stays engaging.
+  """
+  return scenario
 
 def check_row_exists():
   try:
@@ -72,18 +81,20 @@ def get_ai_msg(created_at):
     try:
       difference = (datetime.now() - start_time).total_seconds()
       if difference >= 180:
-        insert_test_result(None, conversationid, conversation, None, "[Error] Huma AI didn't send message in 180s.")
-        print("Timed out error! Your Bot will be turned off soon.")
+        insert_test_result(None, conversationid, conversation, None, f"[Error] {ai_profile['name']} didn't send message in 180s.")
+        print("Timed out error! Bot will be turned off.")
         os._exit(0)
-      response = supabase.table('chat_messages').select('id', 'conversations_id', 'message').eq("sender_id", ai_profile['id']).eq("receiver_id", userid).eq("conversations_id", conversationid).gt('created_at', created_at).execute()
+      response = supabase.table('chat_messages').select('id', 'conversations_id', 'message').eq("sender_id", ai_profile['id']).eq("receiver_id", userid).eq("conversations_id", conversationid).gt('created_at', created_at).order('created_at', desc=True).limit(1).execute()
       supabase.realtime
-      if len(response.data) == 0:
+      if len(response.data) == 0 or len(response.data[0]['message']) == 0:
         continue
       else:
         chat_message = response.data[0]
         break
     except Exception as e:
-      print(f"An unexpected error occurred: {e}")
+      insert_test_result(None, conversationid, conversation, None, f"[Error] {e}")
+      print(f"An unexpected error occurred: {e}\nBot will be turned off.")
+      os._exit(0)
     time.sleep(1)
   return chat_message
 
@@ -92,14 +103,15 @@ async def communicate_with_gpt(prompt) -> str:
   try:
     response = await open_ai.chat.completions.create(
       messages=[
-        {"role": "system", "content": f"Your name is {username}, 32 year old man using a tinder like app to meet with strangers. You've just encountered a {ai_profile['age']} yo {ai_profile['gender']} named {ai_profile['name']}. You want to great warmly and then learn more about {ai_profile['name']}'s personal details. Then you start to flirt by finding common interests. Keep the response concise and to the point."},
+        {"role": "system", "content": scenario},
         {"role": "user", "content": prompt}
       ],
-      model="gpt-4"
+      model="gpt-4",
+      max_tokens=100
     )
     gpt4_msg = response.choices[0].message.content.strip()
   except Exception as e:
-    print(f"An error occured: {e}")
+    print(f"\nAn error occured: {e}")
   return clear_message(gpt4_msg)
 
 def clear_message(generated_msg):
@@ -115,28 +127,40 @@ def clear_message(generated_msg):
 def insert_test_result(result, conversations_id, conversation, chat_messages_id, description):
   supabase.table('test_results').insert({"result": result,"conversations_id": conversations_id, "conversation": conversation, "chat_messages_id": chat_messages_id, "description": description}).execute()
 
+def process_test_result(gpt_response):
+  gpt_response = gpt_response.split("\n")
+  rating = gpt_response[0]
+  description = gpt_response[1]
+  return rating, description  
+
 async def rate_conversation(conversation):
-  prompt = f"Rate the following conversation on how human-like it is on a scale of 1 to 10, where 1 is very robotic and 10 is very human-like. Provide reasoning for your rating.\n\nConversation:\n{conversation}\n\nRating:"
+  prompt = f"""
+  Rate the following conversation on how human-like it is on a scale of 1 to 10, where 1 is very robotic and 10 is very human-like. When providing your rating, consider the following criteria:
+  - Naturalness of the dialogue flow
+  - Use of idiomatic expressions and colloquialisms
+  - Relevance and coherence of responses
+  - Emotional and empathetic responses
+  - Presence of any awkward or unnatural phrasing
+  
+  Please provide your rating as a single number or a decimal (e.g., 9.5, 10, 9, 8.5) without any additional text.
+  
+  Conversation:
+  {conversation}
+  
+  Rating:
+  Reasoning:
+  """
+  
   try:
     response = await open_ai.chat.completions.create(
       messages=[
         {"role": "user", "content": prompt},
       ],
       model="gpt-4",
-      max_tokens=150
+      max_tokens=100
     )
     gpt_response = response.choices[0].message.content
-    gpt_response = gpt_response.split("\n\n")
-    print("response from gpt =>\n", gpt_response)
-    if "/10. " in gpt_response[0]:
-      rating = gpt_response[0].split("/10. ")[0]
-      description = gpt_response[0].split("/10. ")[1]
-    else:
-      if "/10" in gpt_response[0]:
-        rating = gpt_response[0].split("/")[0]
-      else:
-        rating = gpt_response[0]
-      description = gpt_response[1].split(":")[1].strip()
+    rating, description = process_test_result(gpt_response)
   except ValueError:
     rating = None
     description = None
@@ -156,16 +180,28 @@ async def main():
   # Sleep Order
   sleep_order = check_sleep_order()
   
+  global scenario
+  # scenario = f" Your name is {username}, 32 year old man using a tinder like app to meet with strangers."
+  
   global ai_profile
   for ai_profile in ai_profiles:
+    
+    # scenario += f"""
+    # You've just encountered a {ai_profile['age']} year old {ai_profile['gender']} named {ai_profile['name']}.
+    # You want to great warmly and then plan to learn more about {ai_profile['name']}'s personal details.
+    # Then you start to flirt by finding common interests.
+    # Keep the response concise and to the point.
+    # """
+    # Generate a Scenario
+    scenario = generate_scenario()    
     
     # Conversation Description
     print(f"\nThis is a conversation between {username} and {ai_profile['name']}:")
     # Conversation ID and Global It
     global conversationid
     conversationid = get_conversation_id()
-    # User message(You can change as you want.)
-    usermsg = f"Hi {ai_profile['name']}! How are you?" 
+    # The First User message
+    usermsg = f"Hello {ai_profile['name']}! How is everything going?"
     # A Variable To Count Number Of Messages
     count = 0
     # Define a prompt
@@ -176,8 +212,6 @@ async def main():
     
     while(conversationid is not None):
       
-      if count == 5:
-        break
       # Print message
       print(f"\n{username}: ", usermsg)
       # Update prompt
@@ -193,30 +227,36 @@ async def main():
         break
       # Get response from Huma AI
       chat_message = get_ai_msg(result.data[0]['created_at'])
-      generated_msg = chat_message['message'].strip()
+      generated_msg = chat_message['message']
       # If message from huma is not empty
-      if generated_msg.strip() is not None:
+      if len(generated_msg) != 0:
         if generated_msg.startswith("Me:"):
-          insert_test_result(None, conversationid, conversation, chat_message["id"], "[Error] Message from Huma AI starts with 'ME:'")
+          insert_test_result(None, conversationid, conversation, chat_message["id"], f"[Error] Message from {ai_profile['name']} starts with 'ME:'")
           generated_msg = generated_msg.split(":")[1].strip()
         # Print message
-        print(f"\n{ai_profile["name"]}: ", generated_msg)
+        print(f"\n{ai_profile['name']}: ", generated_msg)
         # Update prompt
-        prompt += f"{ai_profile["name"]}:\n{generated_msg}\n"
+        prompt += f"{ai_profile['name']}:\n{generated_msg}\n"
         # Update conversation List
-        conversation += f"{ai_profile["name"]}: {generated_msg}\n"
-        # Get response from LLM(gpt-4) with a prompt
-        usermsg = await communicate_with_gpt(prompt + f"\n{username}:")
+        conversation += f"{ai_profile['name']}: {generated_msg}\n"
+        
         count += 1
+        # Limit number of messages to 10
+        if count == 10:
+          break
+        # Get response from LLM(gpt-4) with a prompt
+        usermsg = ''
+        while(len(usermsg) == 0):
+          usermsg = await communicate_with_gpt(prompt + f"\n{username}:")
         continue
       # If message from huma is empty
       else:
-        insert_test_result(None, conversationid, conversation, chat_message["id"], "[Error] Huma AI sent an empty message!")
-        print("Noe message error occured! Your Bot will be turned off soon.")
+        insert_test_result(None, conversationid, conversation, chat_message["id"], f"[Error] {ai_profile['name']} sent an empty message!")
+        print("No message error occured! Bot will be turned off.")
         # Exit
         os._exit(0)
         
     rating, description = await rate_conversation(conversation)
-    print("Rating: ", rating, f"\nDescription: {description}")
+    print("\nRating: ", rating, f"\n\nDescription: {description}")
     insert_test_result(rating, conversationid, conversation, None, description)
 asyncio.run(main())
